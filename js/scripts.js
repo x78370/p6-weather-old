baccb09b544b592d173807423c68ec233b3b73bd
Andrea Edgar <aedgar1@it-mmc-m10.eastern.ewu.edu>
2014-03-14 11:25:10 -0700

    2

--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,88 @@
 $(document).foundation();
 // Docs at http://simpleweatherjs.com
 
-/* Does your browser support geolocation? */
-if ("geolocation" in navigator) {
-  $('.js-geolocation').show(); 
-} else {
-  $('.js-geolocation').hide();
-}
+// /* Does your browser support geolocation? */
+// if ("geolocation" in navigator) {
+//   $('.js-geolocation').show(); 
+// } else {
+//   $('.js-geolocation').hide();
+// }
 
-/* Where in the world are you? */
-$('.js-geolocation').on('click', function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
-  });
-});
+// /* Where in the world are you? */
+// $('.js-geolocation').on('click', function() {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+//   });
+// });
 
-/* 
-* Test Locations
-* Austin lat/long: 30.2676,-97.74298
-* Austin WOEID: 2357536
-*/
-$(document).ready(function() {
-  loadWeather('Seattle',''); //@params location, woeid
-});
+// /* 
+// * Test Locations
+// * Austin lat/long: 30.2676,-97.74298
+// * Austin WOEID: 2357536
+// */
+// $(document).ready(function() {
+//   loadWeather('Seattle',''); //@params location, woeid
+// });
 
-function loadWeather(location, woeid) {
-  $.simpleWeather({
-    location: location,
-    woeid: woeid,
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';  
+// function loadWeather(location, woeid) {
+//   $.simpleWeather({
+//     location: location,
+//     woeid: woeid,
+//     unit: 'f',
+//     success: function(weather) {
+//       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+//       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+//       html += '<li class="currently">'+weather.currently+'</li>';
+//       html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';  
       
-      $("#weather").html(html);
+//       $("#weather").html(html);
+//     },
+//     error: function(error) {
+//       $("#weather").html('<p>'+error+'</p>');
+//     }
+//   });
+// }
+
+
+
+
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
+$('#getWeather button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather input').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
     },
+  
     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
+      $('body').html('<p>' + error + '</p>');
     }
+  
   });
-}
+  
+  // 3. Reset input value
+  $('#getWeather input').val('');
+  
+});
+
